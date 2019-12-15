8b8171247d69050d060576377d5e04dfcd61d395
= <maxmo@orange.fr>
2019-12-15 22:34:34 +0100

    Initialisation du projet + début premier sprint

---
+++ Pang Remake/Assets/Scripts/Constants/Direction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Direction
+{
+    // Constantes
+
+    // Définition des valeurs possibles
+    public enum DirectionValue : short
+    {
+        LEFT = -1,
+        NO = 0,
+        RIGHT = 1
+    }
+
+
+    // Méthodes
+
+    public static short GetValue(DirectionValue v)
+    {
+        return (short) v;
+    }
+}
