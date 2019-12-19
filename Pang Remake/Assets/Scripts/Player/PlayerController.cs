6460d20d4b1e0817bb1838f3583e6cb6d48cfa0c
Maxime <maxmo@orange.fr>
2019-12-19 01:01:50 +0100

    Déplacement du joueur

--- Pang Remake/Assets/Scripts/Player/PlayerController.cs
+++ Pang Remake/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Attributs
+
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rigidbody;
+    private Vector2 inputs = Vector2.zero;
+
+
+    // Méthodes
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    private void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // On récupére le mouvement souhaité
+
+        inputs = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        // On oriente le personnage en fonction
+
+        if (inputs != Vector2.zero)
+        {
+            transform.forward = new Vector3(0, 0, inputs.x);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + inputs * speed * Time.fixedDeltaTime);
     }
 }
