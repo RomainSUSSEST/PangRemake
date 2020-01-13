5848de81713d38f07ef155472efaced82e8d41c9
Romain SUSSEST <romainsussest@gmail.com>
2020-01-13 15:15:18 +0100

    Bonus Objects HUD

--- Assets/Scripts/BonusObjects/Gameplay/BonusDoubleGrapnel.cs
+++ Assets/Scripts/BonusObjects/Gameplay/BonusDoubleGrapnel.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BonusDoubleGrapnel : GameplayBonusObjects
 {
-    // Méthode
+    #region
+    [SerializeField] Texture m_TextureWeaponIcon;
+    #endregion
 
+    // Méthode
     protected override void ItemIsPickUp(GameObject Player) {
         Player.GetComponent<Shooting>().SetMaxNbrGrapnel(2);
+        HudManager.Instance.m_WeaponIcon.texture = m_TextureWeaponIcon;
     }
 }
--- Assets/Scripts/HudManager.cs
+++ Assets/Scripts/HudManager.cs
@@ -11,6 +11,7 @@ public class HudManager : Manager<HudManager>
     #region Labels & Values
     [SerializeField] Text currentScore;
     [SerializeField] GameObject HUDPanel;
+    [SerializeField] public RawImage m_WeaponIcon;
     #endregion
 
     #region Manager implementation
